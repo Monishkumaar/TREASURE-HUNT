44a3d19a95d1170f96f97376c244479b7af59d76
monish <monishsasikumar27@gmail.com>
2021-04-30 17:54:29 +0530

    first commit

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+textSize(15)
+text("D T B A A E A S", 100,250)
+fill("lightblue")
+text("Hint: To store information !!", 100,270)
+
+fill("white")
+textSize(15)
+text("N T C F O N I U", 700,150)
+fill("lightblue")
+text("Hint: To do something specific !!", 700,170)
+
+}
